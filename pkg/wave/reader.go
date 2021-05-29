88e6ad3342549efc64d9d676c279473d51c06f4b
dzkb <1517445+dzkb@users.noreply.github.com>
2021-05-29 12:42:59 +0200

    Add basic reader implementation

--- pkg/wave/reader.go
+++ pkg/wave/reader.go
@@ -1,1 +1,19 @@
 package wave
+
+import (
+	"io"
+)
+
+func ReadWave(input io.Reader, output chan []byte, bufsize int) error {
+	defer close(output)
+	for {
+		data := make([]byte, bufsize)
+		n, err := input.Read(data)
+		if n > 0 {
+			output <- data[0:n]
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
